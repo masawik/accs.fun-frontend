ef74801cf30f23d146382f170710bcc9fcde8ff1
Rochev Alexandr <velloso3@yandex.ru>
2021-02-01 16:06:52 +0300

    + LoginForm.tsx

---
+++ src/components/LoginForm/LoginForm.tsx
@@ -0,0 +1,103 @@
+import React, {ChangeEvent, FormEvent, useState} from 'react'
+import cn from 'classnames'
+
+const LoginForm: React.FC = () => {
+
+  const [login, setLogin] = useState('')
+  const [loginError, setLoginError] = useState<string | false>(false)
+
+  const [domain, setDomain] = useState('egsabuser.mcdir.ru')
+  const [password, setPassword] = useState('')
+
+  const DOMAINS = [
+    'egsabuser.mcdir.ru',
+    'pook.tk',
+    'egsacc.mcdir.ru',
+    'egsaccount.mcdir.ru',
+    'egsfarm.mcdir.ru'
+  ]
+  const $domains = DOMAINS.map((i) => (
+    <option key={i} value={i}>{i}</option>
+  ))
+
+  const handleLogin = (e: ChangeEvent<HTMLInputElement>): void => {
+    let val = e.target.value
+
+    if (/@/.test(val)) {
+      const [login, domain] = val.split('@')
+      if (DOMAINS.includes(domain)) {
+        setDomain(domain)
+        val = login
+        if (loginError) setLoginError(false)
+      } else {
+        if (!loginError) setLoginError('указанный домен не существует.')
+      }
+    } else {
+      if (loginError) setLoginError(false)
+    }
+
+    setLogin(val)
+  }
+
+  const formSubmit = (e: FormEvent): void => {
+    e.preventDefault()
+    console.log('submitted')
+  }
+
+  return (
+    <form
+      onSubmit={formSubmit}
+      className='col-sm-10 col-md-8 col-lg-6 offset-sm-1 offset-md-2 offset-lg-3 mt-5'
+    >
+      <div className="input-group mb-3">
+        <input
+          value={login}
+          onChange={handleLogin}
+          type="text"
+          className={cn(
+            "form-control col-8",
+            {'is-invalid': loginError}
+          )}
+          id="login"
+          required
+        />
+
+        <span className="input-group-text">@</span>
+
+        <select
+          value={domain}
+          onChange={(e) => {
+            setDomain(e.target.value)
+          }}
+          className="form-select"
+        >
+          {$domains}
+        </select>
+        {loginError && <div className="invalid-feedback">{loginError}</div>}
+
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="password" className="form-label">Password</label>
+        <input
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value)
+          }}
+          type="password"
+          className="form-control"
+          id='password'
+          required
+        />
+      </div>
+
+      <button
+        type="submit"
+        className="btn btn-primary"
+      >Submit
+      </button>
+    </form>
+  )
+}
+
+export default LoginForm
