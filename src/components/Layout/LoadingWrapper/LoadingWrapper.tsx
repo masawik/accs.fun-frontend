587d92bef00961cd07daeeb7878c65e10f521fd0
Rochev Alexandr <velloso3@yandex.ru>
2021-02-08 03:17:18 +0300

    * стилизованы LoadingWrapper

--- src/components/Layout/LoadingWrapper/LoadingWrapper.tsx
+++ src/components/Layout/LoadingWrapper/LoadingWrapper.tsx
@@ -1,18 +1,12 @@
 import React from 'react'
+import styles from './LoadingWrapper.module.css'
+
 
 const LoadingWrapper: React.FC = () => {
   return (
-    <div
-      className='d-flex flex-column justify-content-center align-items-center'
-      style={{height: '100vh'}}
-    >
-      <div
-        className="spinner-border text-info"
-        role="status"
-        style={{height: '75px', width: '75px'}}
-      >
-      </div>
-      <div className='fs-2 mt-2'>
+    <div className={styles.container}>
+      <div className={styles.loader} />
+      <div className={styles.text}>
         Loading...
       </div>
     </div>
